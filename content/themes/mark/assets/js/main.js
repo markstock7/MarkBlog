6a9208b2a243d06d9980dcb2c5182b92174bf818
mark stock <markstock7@hotmail.com>
2016-03-01 23:16:06 +0800

    n

--- content/themes/mark/assets/js/main.js
+++ content/themes/mark/assets/js/main.js
@@ -1,11 +1,9 @@
 seajs.config({
     base: '/',
     paths: {
-        'lib': '/js/lib',
-        // 'monitor': '/js/monitor'
+        // 'lib': '/js/lib',
     },
     alias: {
-        'sticky': '/assets/js/sticky',
         'scrollSpy': '/assets/js/scrollSpy',
         'infinitescroll': '/assets/js/lib/jquery-infinitescroll.min',
         'highlight': '/assets/js/lib/highlight.min.js',
